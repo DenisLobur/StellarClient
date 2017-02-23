4a71579331eb87b3e822de1e66091f784a2ba768
Denis <denisdenisdenis@bigmir.net>
2017-02-23 13:25:25 +0200

    init repository

---
+++ app/src/main/java/stellar/client/den/stellar/MainActivity.java
@@ -0,0 +1,13 @@
+package stellar.client.den.stellar;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
